64893243bfd7d4fa74ed9dccec3d6286907887da
VidyaSagarPagolu <vidya07422@gmail.com>
2026-04-15 13:59:43 +0530

    shell practice

--- 06-array.sh
+++ 06-array.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-FRUITS=("APPLE" "MANGO" "BANANA")
+FRUITS=("APPLE" "MANGO" "GUAVA")
 
-echo "First fruit is:: ${FRUITS[0]}"
-echo "Second fruit is:: ${FRUITS[1]}"
-echo "Third fruit is:: ${FRUITS[2]}"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+echo "Printing the fruit names with their colours"
+
+echo -e "$R First fruit is:: ${FRUITS[0]} $N"
+echo -e "$YSecond fruit is:: ${FRUITS[1]} $N"
+echo -e "$GThird fruit is:: ${FRUITS[2]} $N"
 
 echo "All fruits are:: ${FRUITS[@]}"
