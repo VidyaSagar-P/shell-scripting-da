f8ab121642d355cf6c16fb39f0e71f865aac4851
VidyaSagarPagolu <vidya07422@gmail.com>
2026-04-15 14:03:22 +0530

    shell practice

--- 06-array.sh
+++ 06-array.sh
@@ -5,12 +5,13 @@ FRUITS=("APPLE" "MANGO" "GUAVA")
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
+P="\e[34m"
 N="\e[0m"
 
-echo "Printing the fruit names with their colours"
+echo -e "$P Printing the fruit names with their colours $N"
 
 echo -e "First fruit is::$R ${FRUITS[0]} $N"
 echo -e "Second fruit is::$Y ${FRUITS[1]} $N"
 echo -e "Third fruit is::$G ${FRUITS[2]} $N"
 
-echo "All fruits are:: ${FRUITS[@]}"
+echo "All fruit names are with no colours:: ${FRUITS[@]}"
