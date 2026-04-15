d12229947a4aa5505fe26a3904cb6e489945dc89
VidyaSagarPagolu <vidya07422@gmail.com>
2026-04-15 13:28:45 +0530

    shell

---
+++ 03-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=vidya
+PERSON2=angel
+
+echo "${PERSON1}:: Hi ${PERSON2}"
+echo "${PERSON2}:: hello ${PERSON1}, How are you?"
+echo "${PERSON1}:: I am fine ${PERSON2}, How are you doing"
+echo "${PERSON2}:: doing great ${PERSON1}"
---
+++ 04-variables-args.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "${PERSON1}:: Hi ${PERSON2}"
+echo "${PERSON2}:: hello ${PERSON1}, How are you?"
+echo "${PERSON1}:: I am fine ${PERSON2}, How are you doing"
+echo "${PERSON2}:: doing great ${PERSON1}"
