09080a3630262c15d1ea3412356a06b0bd3bf68a
VidyaSagarPagolu <vidya07422@gmail.com>
2026-04-15 13:36:13 +0530

    shell

--- 05-variable-read.sh
+++ 05-variable-read.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "Please enter your name___ "
+echo "Please enter your name ${USERNAME} "
 read USERNAME
-echo "Entered name is:: ${USERNAME}"
+# echo "Entered name is:: ${USERNAME}"
