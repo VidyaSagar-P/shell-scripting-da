b259631ab180520965d18afa590aa5dd38a5f80b
VidyaSagarPagolu <vidya07422@gmail.com>
2026-04-15 13:52:44 +0530

    shell practice

---
+++ 06-array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+FRUITS=("APPLE" "MANGO" "BANANA")
+
+echo "First fruit is:: ${FRUITS[0]}"
+echo "Second fruit is:: ${FRUITS[1]}"
+echo "Third fruit is:: ${FRUITS[2]}"
+
+echo "All fruits are:: ${FRUITS[@]}"
