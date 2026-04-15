180eaacf73d855639be56b56721e4394f06b37b1
VidyaSagarPagolu <vidya07422@gmail.com>
2026-04-15 13:40:40 +0530

    shell

--- 05-variable-read.sh
+++ 05-variable-read.sh
@@ -2,8 +2,10 @@
 
 echo "Please enter your name "
 read USERNAME
-echo "Entered name is:: ${USERNAME}"
+# echo "Entered name is:: ${USERNAME}"
 
 echo "Please enter the password"
 read -s PASSWORD
-echo "Entered password is:: ${PASSWORD}"
+# echo "Entered password is:: ${PASSWORD}"
+
+echo "entered name and password is:: ${USERNAME} and ${PASSWORD}"
