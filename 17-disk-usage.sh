256fed093f81fb8225d31fba5a6fc33706d7435a
VidyaSagarPagolu <vidya07422@gmail.com>
2026-04-22 13:10:06 +0530

    shell

--- 17-disk-usage.sh
+++ 17-disk-usage.sh
@@ -9,6 +9,6 @@ do
     PARTITION=$(echo "$line" | grep xfs | awk -F " " '{print $NF}')
     if [ $USAGE -gt $DISK_THRESHOLD ]
     then
-        echo "$PARTITION is more than $DISL_THRESHOLD, Current value is::$USAGE"
+        echo "$PARTITION is more than $DISk_THRESHOLD, Current value is::$USAGE"
     fi
 done <<< $DISK_USAGE
